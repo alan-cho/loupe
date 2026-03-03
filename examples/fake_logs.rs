0803e5b3e10ebe5b04c677a6422783b6b8e89ed2
Alan Cho <alancho397@gmail.com>
2026-03-03 15:42:30 -0600

    feat: detect token and colorize line

---
+++ examples/fake_logs.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Log Levels
+    println!("ERROR: This is an error log.");
+    println!("WARNING: This is an warning log.");
+    println!("INFO: This is an info log.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use owo_colors::AnsiColors;
+use owo_colors::{AnsiColors, OwoColorize};
 use regex::Regex;
+use std::io::{self, BufRead};
 use std::sync::LazyLock;
 
 struct Rule {
     regex: Regex,
+    color: AnsiColors,
+}
+
+struct Token {
+    start_position: usize,
+    end_position: usize,
     color: AnsiColors,
 }
 
@@ -25,5 +32,34 @@ static LOGS: LazyLock<Vec<Rule>> = LazyLock::new(|| {
 });
 
 fn main() {
-    println!("Hello, world!");
+    for line in io::stdin().lock().lines() {
+        let line = line.unwrap();
+        match_rules(&LOGS, line);
+    }
+}
+
+fn match_rules(rules: &Vec<Rule>, line: String) {
+    let mut tokens: Vec<Token> = Vec::new();
+    let colorized = line;
+    for rule in rules.iter() {
+        if let Some(m) = rule.regex.find(&colorized) {
+            tokens.push(Token {
+                start_position: m.start(),
+                end_position: m.end(),
+                color: rule.color,
+            })
+        }
+    }
+
+    tokens.sort_by_key(|t| t.start_position);
+    let mut cursor: usize = 0;
+    for token in &tokens {
+        print!("{}", &colorized[cursor..token.start_position]);
+        print!(
+            "{}",
+            (&colorized[token.start_position..token.end_position]).color(token.color)
+        );
+        cursor = token.end_position;
+    }
+    println!("{}", &colorized[cursor..]);
 }
